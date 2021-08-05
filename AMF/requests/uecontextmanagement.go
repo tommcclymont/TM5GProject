b369527538f1338ca2ea70681a8724c9cf4f0afd
tommcclymont <88499750+tommcclymont@users.noreply.github.com>
2021-08-05 16:55:19 +0100

    Update uecontextmanagement.go

--- AMF/requests/uecontextmanagement.go
+++ AMF/requests/uecontextmanagement.go
@@ -3,7 +3,7 @@ package requests
 // creates request to update amf access registration data
 
 import (
-	"TM5GProject/models"
+	"github.com/tommcclymont/TM5GProject/models"
 	"bytes"
 	"context"
 	"encoding/json"
