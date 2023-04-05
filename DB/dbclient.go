63b2cfa6f8e7fd744dc17b1203b3fae5f7543a95
tommcclymont <88499750+tommcclymont@users.noreply.github.com>
2023-04-05 16:12:49 +0100

    Update dbclient.go

--- DB/dbclient.go
+++ DB/dbclient.go
@@ -11,7 +11,7 @@ import (
 func NewDBClient() *mongo.Client {
 
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb+srv://Tom:h7fjn89o@tm5gproject.n1vef.mongodb.net")
+	clientOptions := options.Client().ApplyURI("mongodb+srv://user:pass@tm5gproject.n1vef.mongodb.net")
 
 	// Connect to MongoDB
 	DBClient, err := mongo.Connect(context.TODO(), clientOptions)
